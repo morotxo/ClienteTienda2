562edfeebc0f326f94ab753177b31f69f42b75d0
emejia <emejia@localhost.localdomain>
2015-07-08 09:02:52 -0500

    ccomit busqueda

---
+++ src/java/BeanRequest/MbrProducto.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BeanRequest;
+
+import DAO.DAOProducto;
+import HibernateUtil.HibernateUtil;
+import Pojos.Producto;
+import java.util.List;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+/**
+ *
+ * @author emejia
+ */
+@ManagedBean
+@RequestScoped
+public class MbrProducto {
+    List<Producto>listaProducto ;
+    private Session sesion;
+    private Transaction transaccion; 
+    /**
+     * Creates a new instance of MbrProducto
+     */
+    public MbrProducto() {
+       
+    }
+    public List<Producto> listarProductos(){
+        this.transaccion=null;
+        this.sesion=null;
+        try{
+            DAOProducto daoProducto = new DAOProducto();
+            this.sesion=HibernateUtil.getSessionFactory().openSession();
+            this.transaccion= this.sesion.beginTransaction();
+            this.listaProducto= daoProducto.listar(this.sesion);
+            this.transaccion.commit();
+            return listaProducto;
+        }
+        catch (Exception ex){
+             if(this.transaccion!=null)
+            {
+                this.transaccion.rollback();
+            }
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_FATAL, "Error Fatal:", "por favor contacte con su administrador"));
+            return null;
+        }
+        finally{
+               if(this.sesion!=null)
+            {
+                this.sesion.close();
+            }
+        }
+        
+    }
+    
+
+    public List<Producto> getListaProducto() {
+        return listaProducto;
+    }
+
+    public void setListaProducto(List<Producto> listaProducto) {
+        this.listaProducto = listaProducto;
+    }
+    
+    
+}
---
+++ src/java/Interface/InterfaceProducto.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Interface;
+
+import Pojos.Producto;
+import java.util.List;
+import org.hibernate.Session;
+
+/**
+ *
+ * @author emejia
+ */
+public interface InterfaceProducto {
+    
+    public List<Producto> listar(Session sesion) throws Exception;
+    
+}
